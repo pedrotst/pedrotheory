fc35f7156b143beb9d0f5e1f9c109c95eb4b6936
Pedro Abreu <abreu223@hotmail.com>
2020-05-15 11:58:08 -0400

    Hashtable context

--- src/Vernacular.ml
+++ src/Vernacular.ml
@@ -8,15 +8,20 @@ type statement =
 | Check of exp
 | Eval of exp
 
-type context = (ident * exp) array
+type context = (ident, exp) Hashtbl.t
 
 type program = statement list
 
-let ctx : context = [| |]
+let ctx : context = Hashtbl.create 20
 
 let rec eval_statement s =
 match s with
 | Check e -> printf "%s\n" (pp_exp e); ()
+| Eval e -> printf "%s\n" (pp_exp e); ()
+| Definition (i, e) ->
+    Hashtbl.add ctx i e;
+    printf "%s is Defined!\n" i;
+    printf "There are %d definition(s) in the context\n" (Hashtbl.length ctx)
 | _ -> ()
 
 let eval (ps: program) = List.fold_right (fun s _ -> eval_statement s) ps ()
