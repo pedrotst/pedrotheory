5f4032963ec96308a5bcb89ba881f8943a77203e
Pedro Abreu <abreu223@hotmail.com>
2020-05-15 12:21:52 -0400

    First steps towards REPL

--- src/Main.ml
+++ src/Main.ml
@@ -4,4 +4,12 @@ open Format
 
 let program_of_string s = Parser.parse_program Lexer.lex (Lexing.from_string s)
 
-let main = eval (program_of_string (read_line ()))
+let main =
+  let s = ref "" in
+  s := read_line ();
+  while not (!s = "Quit.") do
+    eval (program_of_string !s);
+    printf "@?" ;
+    flush_all ();
+    s := read_line ()
+  done
